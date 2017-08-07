98f42852e3de181aee6cd32a5c725ff8c0796ca2
monster860 <mnmaxim@gmail.com>
2017-08-06 21:03:05 -0400

    code

---
+++ spec/bluespess-editor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BluespessEditorView from '../lib/bluespess-editor-view';
+
+describe('BluespessEditorView', () => {
+	it('has one valid test', () => {
+		expect('life').toBe('easy');
+	});
+});
