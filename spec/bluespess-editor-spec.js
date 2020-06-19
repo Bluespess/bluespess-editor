f2bc805b1b89e2bceced4cb06282323d22697dba
Taislin <cariosecus@gmail.com>
2020-06-19 01:51:51 +0100

    Revert "prettify"
    This reverts commit 882e348fd94b3b10377be88cb20e23105478a248.

--- spec/bluespess-editor-spec.js
+++ spec/bluespess-editor-spec.js
@@ -1,79 +1,73 @@
-"use strict";
+'use strict';
 
-const BluespessEditor = require("../lib/bluespess-editor");
+const BluespessEditor = require('../lib/bluespess-editor');
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe("BluespessEditor", () => {
-  let workspaceElement, activationPromise;
+describe('BluespessEditor', () => {
+	let workspaceElement, activationPromise;
 
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage("bluespess-editor");
-  });
+	beforeEach(() => {
+		workspaceElement = atom.views.getView(atom.workspace);
+		activationPromise = atom.packages.activatePackage('bluespess-editor');
+	});
 
-  describe("when the bluespess-editor:toggle event is triggered", () => {
-    it("hides and shows the modal panel", () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector(".bluespess-editor")).not.toExist();
+	describe('when the bluespess-editor:toggle event is triggered', () => {
+		it('hides and shows the modal panel', () => {
+			// Before the activation event the view is not on the DOM, and no panel
+			// has been created
+			expect(workspaceElement.querySelector('.bluespess-editor')).not.toExist();
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, "bluespess-editor:toggle");
+			// This is an activation event, triggering it will cause the package to be
+			// activated.
+			atom.commands.dispatch(workspaceElement, 'bluespess-editor:toggle');
 
-      waitsForPromise(() => {
-        return activationPromise;
-      });
+			waitsForPromise(() => {
+				return activationPromise;
+			});
 
-      runs(() => {
-        expect(workspaceElement.querySelector(".bluespess-editor")).toExist();
+			runs(() => {
+				expect(workspaceElement.querySelector('.bluespess-editor')).toExist();
 
-        let bluespessEditorElement = workspaceElement.querySelector(
-          ".bluespess-editor"
-        );
-        expect(bluespessEditorElement).toExist();
+				let bluespessEditorElement = workspaceElement.querySelector('.bluespess-editor');
+				expect(bluespessEditorElement).toExist();
 
-        let bluespessEditorPanel = atom.workspace.panelForItem(
-          bluespessEditorElement
-        );
-        expect(bluespessEditorPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, "bluespess-editor:toggle");
-        expect(bluespessEditorPanel.isVisible()).toBe(false);
-      });
-    });
+				let bluespessEditorPanel = atom.workspace.panelForItem(bluespessEditorElement);
+				expect(bluespessEditorPanel.isVisible()).toBe(true);
+				atom.commands.dispatch(workspaceElement, 'bluespess-editor:toggle');
+				expect(bluespessEditorPanel.isVisible()).toBe(false);
+			});
+		});
 
-    it("hides and shows the view", () => {
-      // This test shows you an integration test testing at the view level.
+		it('hides and shows the view', () => {
+			// This test shows you an integration test testing at the view level.
 
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
+			// Attaching the workspaceElement to the DOM is required to allow the
+			// `toBeVisible()` matchers to work. Anything testing visibility or focus
+			// requires that the workspaceElement is on the DOM. Tests that attach the
+			// workspaceElement to the DOM are generally slower than those off DOM.
+			jasmine.attachToDOM(workspaceElement);
 
-      expect(workspaceElement.querySelector(".bluespess-editor")).not.toExist();
+			expect(workspaceElement.querySelector('.bluespess-editor')).not.toExist();
 
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, "bluespess-editor:toggle");
+			// This is an activation event, triggering it causes the package to be
+			// activated.
+			atom.commands.dispatch(workspaceElement, 'bluespess-editor:toggle');
 
-      waitsForPromise(() => {
-        return activationPromise;
-      });
+			waitsForPromise(() => {
+				return activationPromise;
+			});
 
-      runs(() => {
-        // Now we can test for view visibility
-        let bluespessEditorElement = workspaceElement.querySelector(
-          ".bluespess-editor"
-        );
-        expect(bluespessEditorElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, "bluespess-editor:toggle");
-        expect(bluespessEditorElement).not.toBeVisible();
-      });
-    });
-  });
+			runs(() => {
+				// Now we can test for view visibility
+				let bluespessEditorElement = workspaceElement.querySelector('.bluespess-editor');
+				expect(bluespessEditorElement).toBeVisible();
+				atom.commands.dispatch(workspaceElement, 'bluespess-editor:toggle');
+				expect(bluespessEditorElement).not.toBeVisible();
+			});
+		});
+	});
 });
